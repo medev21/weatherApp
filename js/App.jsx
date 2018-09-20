f1021fda9b430bc49b4633c9d19af48fcfab8e22
medev21 <benmarteen07@gmail.com>
2018-09-20 15:51:09 -0400

    install axios and set up a basic GET request

--- js/App.jsx
+++ js/App.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import Axios from "axios";
 
 class App extends Component {
 
 	render(){
 		const key = process.env.WEATHER_API;
+
+		Axios({
+		  method:'get',
+		  url:'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=' + key
+		})
+		  .then(function(response) {
+		  	console.log(response);
+		});
+
+
 		console.log(key);
 		return (
 			<h1>hello world</h1>
