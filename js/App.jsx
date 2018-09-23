37b304f127335739b0be4e52a8189d1fe5873cfa
medev21 <benmarteen07@gmail.com>
2018-09-23 15:20:05 -0400

    install react suggestions, put fetched cities outside of class

--- js/App.jsx
+++ js/App.jsx
@@ -1,45 +1,122 @@
 import React, { Component } from "react";
 import Axios from "axios";
 
+const cities = [];
+
+fetch("../public/cities.json").then((res) => {
+	return res.json();
+}).then((json) => {
+	let values = json;
+	cities.push(values.cities);
+	// console.log(cities);
+});
+
+const getSuggestions = value => {
+	const inputValue = value.trim().toLowerCase();
+	const inputLength = inputValue.length;
+
+	return inputLength === 0 ? [] : languages.filter(lang =>
+		lang.name.toLowerCase().slice(0, inputLength) === inputValue
+	);
+};
+
 class App extends Component {
 
 	constructor(props){
 		super(props);
 		this.state = {
-			cityOptions: []
+			cityOptions: [],
+			value: '',
+			suggestedCities: []
 		}
 	}
 
 
-	componentDidMount(){
-		this.fetchCities();
-		console.log(this.state.cityOptions);
-	}
+	// componentDidMount(){
+	// 	// this.fetchCities();
+	// }
 
-	//functions
-	fetchCities(){
-		fetch("../public/cities.json").then((res) => {
-			return res.json();
-		}).then((json) => {
-			let values = json;
-			this.setState({cityOptions: values.cities})
-			console.log(values);
-		});
-	}
+	// //functions
+	// fetchCities = () => {
+	// 	fetch("../public/cities.json").then((res) => {
+	// 		return res.json();
+	// 	}).then((json) => {
+	// 		let values = json;
+	// 		this.setState({cityOptions: values.cities})
+	// 		console.log(values);
+	// 	});
+	// };
+
+	// // Teach Autosuggest how to calculate suggestions for any given input value.
+	// const getSuggestions = value => {
+	//   const inputValue = value.trim().toLowerCase();
+	//   const inputLength = inputValue.length;
+
+	//   return inputLength === 0 ? [] : languages.filter(lang =>
+	//     lang.name.toLowerCase().slice(0, inputLength) === inputValue
+	//   );
+	// };
+
+	// // When suggestion is clicked, Autosuggest needs to populate the input
+	// // based on the clicked suggestion. Teach Autosuggest how to calculate the
+	// // input value for every given suggestion.
+	// const getSuggestionValue = suggestion => suggestion.name;
+
+	// // Use your imagination to render suggestions.
+	// const renderSuggestion = suggestion => (
+	//   <div>
+	//     {suggestion.name}
+	//   </div>
+	// );
+
+	// onChange = (event, { newValue }) => {
+	//     this.setState({
+	//       value: newValue
+	//     });
+	// };
+
+	// // Autosuggest will call this function every time you need to update suggestions.
+	// // You already implemented this logic above, so just use it.
+	// onSuggestionsFetchRequested = ({ value }) => {
+	// 	this.setState({
+	//       suggestions: getSuggestions(value)
+	//     });
+	// };
+
+	// // Autosuggest will call this function every time you need to clear suggestions.
+	// onSuggestionsClearRequested = () => {
+	//     this.setState({
+	//       suggestions: []
+	// });
+
+
 	render(){
-		const key = process.env.WEATHER_API;
+		// const key = process.env.WEATHER_API;
+		// const { value, suggestedCities } = this.state;
 
-		// Axios({
-		//   method:'get',
-		//   url:'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=' + key
-		// })
-		//   .then(function(response) {
-		//   	console.log(response);
-		// });
+	 //    // Autosuggest InputProps
+	 //    const inputProps = {
+	 //      placeholder: 'Type a programming language',
+	 //      value,
+	 //      onChange: this.onChange
+	 //    };
+
+	    // Finally, render it!
+	    return (
+	    	<h1>hello world</h1>
+	      /*<Autosuggest
+	        suggestions={suggestedCities}
+	        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+	        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+	        getSuggestionValue={getSuggestionValue}
+	        renderSuggestion={renderSuggestion}
+	        inputProps={inputProps}
+	      />*/
+	    )
 
 
 
-		return (
+		/*return (
 			<div>
 				<h1>hello world</h1>
 				<select>{
@@ -48,7 +125,7 @@ class App extends Component {
 					)
 				}</select>
 			</div>
-		)
+		)*/
 	}
 }
 
