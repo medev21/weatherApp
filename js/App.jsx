660d8406662ae3951d62e73110269db85bb39d12
medev21 <benmarteen07@gmail.com>
2018-10-01 21:18:28 -0400

    add handleOnChange function in app.jsx

--- js/App.jsx
+++ js/App.jsx
@@ -1,50 +1,9 @@
 import React, { Component } from "react";
-import Axios from "axios";
-import Autosuggest from 'react-autosuggest';
 import CityWeather from './components/CityWeather';
 import SearchWeather from './components/SearchWeather';
 
 
-let cities;
 
-Axios.get("./public/cities.json").then((res) => {
-	if(res.status = 200){
-		let values = res;
-		cities = values.data.cities;	
-	}else{
-		console.log("something went wrong");
-	}
-	
-}).catch((error) => {
-	console.log(error);
-});
-
-// fetch("../public/cities.json").then((res) => {
-// 	return res.json();
-// }).then((json) => {
-// 	let values = json;
-// 	cities = values.cities;	
-// });
-
-const getSuggestions = value => {
-	const inputValue = value.trim().toLowerCase();
-	const inputLength = inputValue.length;
-
-	return inputLength === 0 ? [] : cities.filter(city =>
-		
-		city.name.toLowerCase().slice(0, inputLength) === inputValue
-	);
-};
-
-// When suggestion is clicked, Autosuggest needs to populate the input
-// based on the clicked suggestion. Teach Autosuggest how to calculate the
-// input value for every given suggestion.
-const getSuggestionValue = suggestion => suggestion.name;
-
-// Use your imagination to render suggestions.
-const renderSuggestion = suggestion => (
-  <span>{suggestion.name}</span>
-);
 
 class App extends Component {
 
@@ -58,113 +17,24 @@ class App extends Component {
 		}
 	};
 
-
-	componentDidMount(){
-		// this.fetchCityWeather();
-	}
-
-	onChange = (event, { newValue }) => {
-	    this.setState({
-	      value: newValue
-	    });
-	};
-
-	// Autosuggest will call this function every time you need to update suggestions.
-	// You already implemented this logic above, so just use it.
-	onSuggestionsFetchRequested = ({ value }) => {
+	handleCityWeatherData = (city) => {
 		this.setState({
-	      suggestedCities: getSuggestions(value)
+			cityWeatherData: city
+		});
+	};
+
+	handleOnChange = (newValue) => {
+		this.setState({
+	    	value: newValue
 	    });
-	};
-
-	// Autosuggest will call this function every time you need to clear suggestions.
-	onSuggestionsClearRequested = () => {
-	    this.setState({
-	      suggestedCities: []
-		});
-	};
-
-	renderSuggestionsContainer = ({ containerProps, children, query }) => {
-		return (
-	    	<div {...containerProps}>
-	    		{children}
-	    		<h5>I like showing up.</h5>
-	    	</div>
-	    );
-	};
-
-	fetchCityWeather = (cityId) => {
-		//fetching sample request
-		Axios.get("/public/sampleWeather.json").then((response) => {
-			if(response.status === 200){
-				this.setState({
-					cityWeatherData: response.data
-				});
-			}
-			else{
-				console.log('fetchCityWeather - something went wrong');
-			}
-			
-		})
-		.catch((error) => {
-			console.log(error);
-		});
-
-		//fetching Openweather map
-		// const weatherKey = process.env.WEATHER_API;
-		// Axios.get('http://api.openweathermap.org/data/2.5/forecast',{
-		// 	params: {
-		// 		id: cityId,
-		// 		APPID: weatherKey
-		// 	}
-		// })
-		// .then((response) => {
-		// 	console.log(response);
-		// })
-		// .catch((error) => {
-		// 	console.log(error);
-		// })
-	};
-
-	onSuggestionSelected = (event, { suggestion, suggestionValue, suggestionIndex, sectionIndex, method }) => {
-		console.log(suggestion);
-		console.log(method);
-
-		if(method == 'click'){
-			let cityId = suggestion.id;
-			this.fetchCityWeather(cityId);
-		}
-		
 	}
 
 	render() {
-		const { value, suggestedCities } = this.state;
-
-	    // Autosuggest InputProps
-	    const inputProps = {
-	      placeholder: 'Type your city',
-	      value,
-	      onChange: this.onChange
-	    };
-
-
 
 	    // Finally, render it!
 	    return (
 	    	<div>
-	    		<SearchWeather />
-			    <Autosuggest
-			    	suggestions={suggestedCities}
-			        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-			        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-			        getSuggestionValue={getSuggestionValue}
-			        renderSuggestion={renderSuggestion}
-			        inputProps={inputProps} 
-			        shouldRenderSuggestions = {(v) => v.trim().length > 0}
-			        renderSuggestionsContainer={this.renderSuggestionsContainer}
-			        onSuggestionSelected={this.onSuggestionSelected}
-			    />
-
+	    		<SearchWeather suggestData={{value: this.state.value, suggestedCities: this.state.suggestedCities}} onSelectCity={this.handleCityWeatherData} onChange={this.handleOnChange}/>
 			    <CityWeather weatherData={this.state.cityWeatherData}/>
 		    </div>
 	    );
--- js/components/SearchWeather.jsx
+++ js/components/SearchWeather.jsx
@@ -1,11 +1,153 @@
 import React, { Component } from "react";
+import Axios from "axios";
+import Autosuggest from 'react-autosuggest';
 
+// let cities;
+
+// fetch("../public/cities.json").then((res) => {
+// 	return res.json();
+// }).then((json) => {
+// 	let values = json;
+// 	cities = values.cities;	
+// });
+
+const getCities = () => {
+	Axios.get("./public/cities.json").then((res) => {
+		return res;
+	}).then((json) => {
+		console.log(json.data.cities);
+		return json.data.cities;
+	}).catch((error) => {
+		console.log(error);
+	});
+};
+
+const getSuggestions = value => {
+	const inputValue = value.trim().toLowerCase();
+	const inputLength = inputValue.length;
+
+	const cities = getCities();
+
+	console.log(cities);
+
+
+	return inputLength === 0 ? [] : cities.filter(city =>
+		
+		city.name.toLowerCase().slice(0, inputLength) === inputValue
+	);
+};
+
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+const getSuggestionValue = suggestion => suggestion.name;
+
+// Use your imagination to render suggestions.
+const renderSuggestion = suggestion => (
+  <span>{suggestion.name}</span>
+);
 
 class SearchWeather extends Component {
 
+	onChange = (event, { newValue }) => {
+	    this.props.onChange(newValue);
+	};
+
+	// Autosuggest will call this function every time you need to update suggestions.
+	// You already implemented this logic above, so just use it.
+	onSuggestionsFetchRequested = ({ value }) => {
+		this.setState({
+	      suggestedCities: getSuggestions(value)
+	    });
+	};
+
+	// Autosuggest will call this function every time you need to clear suggestions.
+	onSuggestionsClearRequested = () => {
+	    this.setState({
+	      suggestedCities: []
+		});
+	};
+
+	renderSuggestionsContainer = ({ containerProps, children, query }) => {
+		return (
+	    	<div {...containerProps}>
+	    		{children}
+	    		<h5>I like showing up.</h5>
+	    	</div>
+	    );
+	};
+
+	fetchCityWeather = (cityId) => {
+		//fetching sample request
+		Axios.get("/public/sampleWeather.json").then((response) => {
+			if(response.status === 200){
+				return response.data
+			}
+			else{
+				console.log('fetchCityWeather - something went wrong');
+			}
+			
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+
+		//fetching Openweather map
+		// const weatherKey = process.env.WEATHER_API;
+		// Axios.get('http://api.openweathermap.org/data/2.5/forecast',{
+		// 	params: {
+		// 		id: cityId,
+		// 		APPID: weatherKey
+		// 	}
+		// })
+		// .then((response) => {
+		// 	console.log(response);
+		// })
+		// .catch((error) => {
+		// 	console.log(error);
+		// })
+	};
+
+	onSuggestionSelected = (event, { suggestion, suggestionValue, suggestionIndex, sectionIndex, method }) => {
+		console.log(suggestion);
+		console.log(method);
+
+		if(method == 'click'){
+			let cityId = suggestion.id;
+			let data = this.fetchCityWeather(cityId);
+			this.props.onSelectCity(data); //pass data to parent
+		}
+	};
+
+
+	componentDidMount = () => {
+		console.log('componentDidMount');
+	}
+
+
 	render(){
+		const value = this.props.suggestData.value;
+		const suggestedCities = this.props.suggestData.suggestedCities;
+
+	    // Autosuggest InputProps
+	    const inputProps = {
+	      placeholder: 'Type your city',
+	      value,
+	      onChange: this.onChange
+	    };
+
 		return(
-			<p>SearchWeather</p>
+			<Autosuggest
+		    	suggestions={suggestedCities}
+		        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+		        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+		        getSuggestionValue={getSuggestionValue}
+		        renderSuggestion={renderSuggestion}
+		        inputProps={inputProps} 
+		        shouldRenderSuggestions = {(v) => v.trim().length > 0}
+		        renderSuggestionsContainer={this.renderSuggestionsContainer}
+		        onSuggestionSelected={this.onSuggestionSelected}
+		    />
 		);
 	}
 }
