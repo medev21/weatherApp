5a7f24b5c97b184b3dc2921b425e5133af472a4e
medev21 <benmarteen07@gmail.com>
2018-10-07 15:07:27 -0400

    convert kelvin temps to fahrenheit

--- js/components/CityWeather.jsx
+++ js/components/CityWeather.jsx
@@ -1,16 +1,27 @@
 //this is the cityweathercomponent
 import React, { Component } from "react";
-import apis from "../utils/apis";
 
 class CityWeather extends Component {
 
-	// getFahrenheit = () => {
+	// (280.89K − 273.15) × 9/5 + 32
 
-	// };
+	getFahrenheit = (now, minimum, maximum) => {
 
-	// componentWillMount = () => {
-	// 	console.log("mounted after click");
-	// };
+		let arr = [now , minimum , maximum];
+		let result = [];
+
+		for (let value of arr) {
+			let num = (value - 273.15) * 9/5 + 32;
+			result.push(num);
+		}
+		let temp = result[0];
+		let minTemp = result[1];
+		let maxTemp = result[2];
+
+		console.log(result);
+
+		return {temp, minTemp, maxTemp};
+	};
 
 	render(){
 
@@ -18,31 +29,13 @@ class CityWeather extends Component {
 		let weatherData = this.props.weatherData;
 
 		let currentWeather = weatherData.list[0];
-		let temp = currentWeather.main.temp;
-		let maxTemp = currentWeather.main.temp_max;
-		let minTemp = currentWeather.main.temp_min;
 		let condition = currentWeather.weather[0].main;
 
-		console.log("cityWeather render", currentWeather);
-		console.log("cityWeather render", temp);
-		console.log("cityWeather render", maxTemp);
-		console.log("cityWeather render", minTemp);
-		console.log("cityWeather render", condition);
 
+		let {temp, minTemp, maxTemp} = this.getFahrenheit(currentWeather.main.temp, currentWeather.main.temp_min, currentWeather.main.temp_max);
 
-		// if(cityWeatherData.length != 0){
-		// 	let city = cityWeatherData.city;
-		// 	let today = cityWeatherData.list[0];
-
-		// 	let temp = today.main.temp;
-		// 	let tempMax = today.main.temp_max;
-		// 	let tempMin = today.main.temp_min;
-		// 	let weather = today.weather[0].main;
-		// 	console.log(today);
-		// 	console.log(weather);
-		// }
-		
-
+		// console.log(temp);
+		console.log(temp, minTemp, maxTemp);
 
 		return(
 			<div>
@@ -68,15 +61,3 @@ class CityWeather extends Component {
 }
 
 export default CityWeather;
-
-
-			// 	<br>
-			// 	<h2>current temp</h2>
-			// 	<p>{temp}</p>
-			// </div>
-			// 	<br>
-			// 	<h2>max temp</h2>
-			// 	<p>{maxTemp}</p>
-			// 	<br>
-			// 	<h2>min temp</h2>
-			// 	<p>{minTemp}</p>
