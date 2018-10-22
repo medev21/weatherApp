1a5c9cdf4530825cb95a86c53dc41b0f5e47ce82
medev21 <benmarteen07@gmail.com>
2018-10-22 08:04:31 -0400

    style weather info

--- js/components/weatherIcons.jsx
+++ js/components/weatherIcons.jsx
@@ -23,7 +23,7 @@ class WeatherIcon extends Component {
 		let path = this.getPath(this.props.code)
 
 		return(
-			<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" height="180"
+			<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" height="150"
 	 viewBox="0 0 30 30" style={{enableBackground:"new 0 0 30 30"}} xmlSpace="preserve">
 			<path style={{fill: "#fff"}} d={path}/>
 		</svg>
