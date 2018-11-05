d9c36a9236ac231ad234220dc1dfde4d1c8f55eb
medev21 <benmarteen07@gmail.com>
2018-11-05 15:28:48 -0500

    create server js

---
+++ server.js
@@ -0,0 +1,14 @@
+//server.js
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 5050;
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('*', (req,res) => {
+	res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.listen(port);
+console.log('Server started');
