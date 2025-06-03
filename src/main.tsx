8e880c14ab437c930bed10efba2e4389da72c6d1
medev21 <benmarteen07@gmail.com>
2025-06-03 11:14:09 -0400

    configure query client

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,28 @@
+// COMPONENTS
+import App from 'src/App'
+// DEPENDENCIES
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+// STYLES
 import 'src/style/universal.scss';
 import 'src/style/app.scss';
-import App from 'src/App'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		}
+	}
+})
 
 createRoot(document.getElementById("weatherapp")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
+	</StrictMode>,
 )
